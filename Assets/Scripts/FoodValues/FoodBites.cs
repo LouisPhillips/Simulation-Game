54fe574ec6941b850d89ba1b1a9231dfc59def57
Louis Phillips <Louis2.Phillips@live.uwe.ac.uk>
2023-02-12 17:13:36 +0000

    Week 1
    - Base class

---
+++ Assets/Scripts/FoodValues/FoodBites.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBites : MonoBehaviour
+{
+    public int bites = 3;
+    public float foodValue = 20f;
+
+    private void Update()
+    {
+        
+    }
+}
